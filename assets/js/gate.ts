3cd3aebaa9eb97294386a4ba91cc345b0cc3518f
Tomáš Vojík <vojik@wboy.cz>
2024-02-01 19:00:48 +0100

    :recycle: Refactored JS

--- assets/js/gate.ts
+++ assets/js/gate.ts
@@ -1,6 +1,6 @@
 import EventServerInstance from "../../../../assets/js/EventServer";
 import {loadContent, tipsRotations} from "../../../../assets/js/components/gate";
-import {gameTimer} from "../../../../assets/js/includes/functions";
+import {gameTimer} from '../../../../assets/js/includes/gameTimer';
 
 export let reloadTimeout: { timeout: null | ReturnType<typeof setTimeout> } = {timeout: null};
 window.addEventListener('load', () => {
