3b27ab0048d94192780f963d3523c93fc053761b
Tomáš Vojík <vojik@wboy.cz>
2024-10-02 13:46:19 +0200

    :recycle: New FontawesomeManager to auto-register fontawesome icons

--- src/Services/MenuExtension.php
+++ src/Services/MenuExtension.php
@@ -2,17 +2,23 @@
 
 namespace LAC\Modules\Tournament\Services;
 
+use App\Services\FontAwesomeManager;
 use LAC\Modules\Core\MenuExtensionInterface;
 
 class MenuExtension implements MenuExtensionInterface
 {
+    public function __construct(
+        private readonly FontAwesomeManager $fontawesome,
+    ) {
+    }
+
     /**
      * @inheritDoc
      */
     public function extend(array &$menu): void {
         $menu['tournaments'] = [
             'name' => lang('Turnaje'),
-            'icon' => 'fa-solid fa-trophy',
+            'icon' => $this->fontawesome->solid('trophy'),
             'route' => 'tournaments',
             'order' => 50,
         ];
