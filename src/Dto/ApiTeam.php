f7d2d61cd9d2597e6799b8ec998b0f979715674e
Tomáš Vojík <vojik@wboy.cz>
2024-11-01 16:09:48 +0100

    :bug: Fixed ApiPlayer deserialization

--- src/Dto/ApiTeam.php
+++ src/Dto/ApiTeam.php
@@ -1,11 +1,31 @@
 <?php
+
 declare(strict_types=1);
+
 namespace LAC\Modules\Tournament\Dto;
 
-class ApiTeam {
+class ApiTeam
+{
     public int $id;
     public string $name;
     public ?string $image = null;
     /** @var ApiPlayer[] */
     public array $players = [];
+
+    public function addPlayer(ApiPlayer $player): void {
+        $this->players[] = $player;
+    }
+
+    public function removePlayer(ApiPlayer $player): void {
+        foreach ($this->players as $key => $test) {
+            if ($test === $player) {
+                unset($this->players[$key]);
+                return;
+            }
+        }
+    }
+
+    public function hasPlayers(): bool {
+        return count($this->players) > 0;
+    }
 }
